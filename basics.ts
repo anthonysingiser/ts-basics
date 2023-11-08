448a62e1b67298e6b7ee611968508c1c35285c66
Anthony <asingiser@gmail.com>
2023-11-08 08:39:20 -0500

    set up environment for typescript learning

---
+++ basics.ts
@@ -0,0 +1,11 @@
+let userName: string = "Anthony";
+let hasLoggedIn: boolean = true;
+
+hasLoggedIn += " Singiser";
+
+console.log(userName);
+
+let myNumber: number = 10;
+
+let myRegex: RegExp = /foo/;
+
