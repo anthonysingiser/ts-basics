34437bb7c2813ba34fb53e01d7022153229c33e7
chrysanthemum <asingiser@gmail.com>
2023-11-12 09:27:48 -0500

    typing functionswith functions

---
+++ funcs-and-funcs.ts
@@ -0,0 +1,26 @@
+//typing a function that returns a function
+export function printToFile(text: string, callback: () => void): void {
+    callback()
+}
+
+//create type for a function that takes a number as a parameter and returns a number
+type MutationFunction = (v: number) => number
+
+export function arrayMutate(
+    numbers: number[], 
+    mutate: MutationFunction
+): number[] {
+    return numbers.map(mutate)    
+}
+//declaring a new function and using the MutationFunction type
+const myNewMutateFunc: MutationFunction = (v: number) => v * 100
+
+console.log(myNewMutateFunc(10))
+console.log(arrayMutate([1,20,3], (v) => v * 10))
+
+export function createAdder(num: number): (val: number) => number {
+    return (val: number) => num + val
+}
+
+const addOne = createAdder(1)
+console.log(addOne(55))
