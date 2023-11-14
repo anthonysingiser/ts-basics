dd4fa8ee9264e9eea618876f8ca7e0c4e977a7da
chrysanthemum <asingiser@gmail.com>
2023-11-14 11:25:56 -0500

    add ability to accept strings

--- parseCoordinate.ts
+++ parseCoordinate.ts
@@ -18,7 +18,7 @@ function parseCoordinateFromNumbers(x: number, y: number): Coordinate {
 
 //function parseCoordinate(obj: Coordinate): Coordinate;
 //function parseCoordinate(x: number, y: number): Coordinate;
-
+//function parseCoordinate(str: string): Coordinate;
 //typing a function parseCoordinate that is capable of accepting one object or two numbers as arguments
 
 function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
@@ -26,7 +26,12 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
         x: 0,
         y: 0,
     };
-    if (typeof arg1 === 'object') {
+    if (typeof arg1 === 'string') {
+        (arg1 as string).split(',').forEach((str) => {
+          const [key, value] = str.split(':');
+          coord[key as "x" | "y"] = parseInt(value, 10);
+        });
+    } else if (typeof arg1 === 'object') {
         coord = {
             ...(arg1 as Coordinate)
         }
@@ -41,3 +46,4 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
 
 console.log(parseCoordinate(10, 20))
 console.log(parseCoordinate({x: 34, y:65}))
+console.log(parseCoordinate("x:12,y:22"))
