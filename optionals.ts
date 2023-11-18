5ddf0d62ef405facd6afefd49f70f353033fe126
chrysanthemum <asingiser@gmail.com>
2023-11-18 09:34:53 -0500

    finish lesson

--- optionals.ts
+++ optionals.ts
@@ -4,3 +4,26 @@ function printIngredient(quantity: string, ingredient: string, extra?: string) {
 
 printIngredient("1 Cup", "Flour")
 printIngredient("1 Cup", "Flour", "Butter")
+
+interface User {
+    id: string
+    info?: {
+        email?: string
+    }   
+}
+
+function getEmail(user: User): string {
+    if (user.info) {
+        return user.info.email!
+    }
+    return ""
+}
+
+function getEmailEasy(user: User): string {
+    return user?.info?.email ?? ""
+}
+
+function addWithCallback(x:number, y:number, callback: () => void) {
+    console.log(x,y)
+    callback?.()
+}
