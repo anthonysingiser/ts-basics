fbe7c54351915c98b294871b7967caaa7c326515
chrysanthemum <asingiser@gmail.com>
2023-11-26 10:14:18 -0500

    comments on lesson

--- readonly-1.ts
+++ readonly-1.ts
@@ -11,13 +11,18 @@ function makeCat(name: string, breed: string): Readonly<Cat> {
 }
 
 const mittens = makeCat("Mittens", "Tabby")
-//mittens.name = "Joe"
+//mittens.name = "Joe" // error, the object is immutable after adding 'Readonly' to the return type of makeCat
 
-function makeCoordinate(x: number, y: number, z: number) {
+function makeCoordinate(
+    x: number, 
+    y: number, 
+    z: number
+): readonly [number, number, number] {
     return [x, y, z]
 }
 
 const c1 = makeCoordinate(10, 20, 30 )
-c1[0] = 50
+//c1[0] = 50 // error, after adding 'readonly' to the return type of makeCoordinate, the array is immutable
 
-const reallyConst = [1,2,3]
+const reallyConst = [1,2,3] as const
+//reallyConst[0] = 50 // error, now that we have added 'as const' to the array, it is immutable
