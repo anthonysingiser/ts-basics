e7117da7ed34008d67bec244d6deb4b08bea2741
chrysanthemum <asingiser@gmail.com>
2023-12-02 09:45:43 -0500

    add lessons type inference, rest parameters, spread syntax

---
+++ arrayTypeInference.ts
@@ -0,0 +1,10 @@
+// Don't change this part:
+let dogTup: [string, string, string, string] = ['dog', 'brown fur', 'curly tail', 'sad eyes'];
+
+// Your code goes here:
+//define a variable myArr with type string[],
+//without using: Array, or []
+
+const myArr = dogTup.concat('new string')
+
+myArr[50]= 'not a dog'
