71c4bf75a7897afd9f1da2637e3628839edc23f4
chrysanthemum <asingiser@gmail.com>
2023-12-06 08:12:45 -0500

    implement getMaxPrice function

--- restaurant-orders-project/index.ts
+++ restaurant-orders-project/index.ts
@@ -1,3 +1,21 @@
 import { restaurants, Restaurant } from "./restaurants";
 import { orders, Order, PriceBracket } from "./orders";
 
+function getMaxPrice(bracket: PriceBracket){
+    switch(bracket) {
+      case PriceBracket.Low: {
+        return 10
+      }
+      case PriceBracket.Medium: {
+        return 20
+      }
+      case PriceBracket.High: {
+        return 30
+      }
+      default : {
+        return "Error getMaxPrice: Not a valid PriceBracket"
+      }
+    }
+  }
+
+  console.log(getMaxPrice(PriceBracket.Medium))
