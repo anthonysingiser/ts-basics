0c3695af56d9e1ea6a5bb952e2b20ec1b2c05cfe
Anthony <asingiser@gmail.com>
2023-12-12 11:16:24 -0500

    finish project

--- unionversity/index.ts
+++ unionversity/index.ts
@@ -23,5 +23,27 @@ type SearchEventsOptions = {
 }
 
 function searchEvents(options: SearchEventsOptions) {
-    const events = options.eventType === 'course' ? courses : studyGroups
+    const events: (Course | StudyGroup)[] = options.eventType === 'course' ? courses : studyGroups
+    return events.filter((event: Course | StudyGroup) => {
+        if (typeof options.query === 'number') {
+            return options.query === event.id
+        }
+        if (typeof options.query === 'string') {
+            return event.keywords.includes(options.query)
+        }
+    })
 }
+
+const searchResults = searchEvents({ query: 'science', eventType: 'course' })
+
+console.log(searchResults)
+
+const enrolledEvents: (Course | StudyGroup)[] = []
+
+function enroll(event: Course | StudyGroup) {
+    enrolledEvents.push(event)
+}
+
+enroll(searchResults[0])
+
+console.log(enrolledEvents)
