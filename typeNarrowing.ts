8d91718910fbb954429392bbb5c3bcfdfef636a5
chrysanthemum <asingiser@gmail.com>
2023-12-14 10:53:58 -0500

    finish type narrowing lesson

--- typeNarrowing.ts
+++ typeNarrowing.ts
@@ -41,3 +41,60 @@ function formatStatistic(stat: string | number) {
   
   console.log(move(siameseCat))
   console.log(move(bettaFish))
+
+  type Pasta = {
+    menuName: string;
+    boil: () => string;
+  }
+  
+  type Meat = {
+    menuName: string;
+    panFry: () => string;
+  }
+  
+  const fettuccine = {
+    menuName: 'Fettuccine',
+    boil: () => 'Heat water to 212 degrees',
+  }
+  
+  const steak = {
+    menuName: 'New York Strip Steak',
+    panFry: () => 'Heat oil to 350 degrees',
+  }
+  
+  function prepareEntree(entree: Pasta | Meat) {
+    if('boil' in entree){
+      return entree.boil();
+    }
+    else {
+      return entree.panFry();
+    }
+  }
+  
+  console.log(prepareEntree(fettuccine));
+
+  type Metal = {
+    magnetize: () => string;
+  }
+  
+  type Glass = {
+    melt: () => string;
+  }
+  
+  const iron = {
+    magnetize: () => 'Electromagnet activated'
+  }
+  
+  const bottle = {
+    melt: () => 'Furnace set to 2,700 degrees'
+  }
+  
+  
+  function recycle(trash: Metal | Glass) {
+    if('magnetize' in trash){
+      return trash.magnetize()
+    }
+    return trash.melt();
+  }
+  
+  console.log(recycle(iron));
