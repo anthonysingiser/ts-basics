14e709eae1571b1891d40e05931f23a8f78d0f40
Anthony <asingiser@gmail.com>
2023-12-24 10:29:49 -0500

    optional type members

--- advancedObjectTypes.ts
+++ advancedObjectTypes.ts
@@ -84,12 +84,39 @@ me.code();
 //index signatures
 
 interface Budget {
-  [category: string]: number
+    [category: string]: number
 }
 
 async function getBudget() {
-  const result: Budget = await getBudgetAsync();
-  console.log(result);
+    const result: Budget = await getBudgetAsync();
+    console.log(result);
 }
 
 getBudget();
+
+// optional type members
+
+interface UserNameOptions {
+    firstName?: string;
+    lastName?: string;
+    username: string;
+}
+
+function getUserName(options: UserNameOptions) {
+    if (options.firstName && options.lastName) {
+        return console.log(`${options.firstName} ${options.lastName}`);
+    }
+
+    return console.log(options.username);
+}
+
+getUserName({
+    firstName: 'Mr.',
+    lastName: 'Oshiro',
+    username: 'hotelowner304'
+})
+
+getUserName({
+    firstName: 'Madeline',
+    username: 'mountainClimber'
+})
