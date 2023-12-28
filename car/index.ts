e690b3c0eea95c0d4b66959ef15c4badc6672b22
chrysanthemum <asingiser@gmail.com>
2023-12-28 09:17:45 -0500

    add steering

--- car/index.ts
+++ car/index.ts
@@ -1,30 +1,50 @@
 import { getObstacleEvents } from "./computer-vision";
 
 interface AutonomousCar {
-    isRunning: boolean;
-    respond: (events: Events) => void;
+  isRunning: boolean;
+  respond: (events: Events) => void;
 }
 
 interface AutonomousCarProps {
-    isRunning?: boolean;
+  isRunning?: boolean;
 }
 
 interface Events {
-    [event: string]: boolean;
+  [event: string]: boolean;
+}
+
+interface Control {
+  execute: (command: string) => void;
+}
+
+interface Steering extends Control {
+  turn: (direction: string) => void;
+}
+
+class SteeringControl implements Steering {
+  execute(command: string) {
+    console.log("Executing: " + command);
+  }
+  turn(direction: string) {
+    this.execute("turn " + direction);
+  }
 }
 
 class Car implements AutonomousCar {
-    isRunning;
-    constructor(props: AutonomousCarProps) {
-        this.isRunning = props.isRunning;
+  isRunning;
+  constructor(props: AutonomousCarProps) {
+    this.isRunning = props.isRunning;
+  }
+  respond(events: Events) {
+    if (this.isRunning === false) {
+      return "car is not running";
     }
-    respond(events: Events) {
-        if (this.isRunning === false) {
-            return "car is not running";
-        }
-        return;
-    }
+    return;
+  }
 }
+
+const steering = new SteeringControl();
+steering.turn("right");
 
 const autonomousCar = new Car({ isRunning: false });
 console.log(autonomousCar.respond(getObstacleEvents()));
